d2a7b2f624bbb3c78caaa231ed1d0bed24c6026c
Patrick Lamoureux <42650486+sophomorica@users.noreply.github.com>
2026-08-25 02:52:48 -0400

    Add SvelteKit project config

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	compilerOptions: {
+		runes: ({ filename }) =>
+			filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+	},
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			origin: 'https://nrscktrade.vercel.app'
+		}
+	}
+};
+
+export default config;
